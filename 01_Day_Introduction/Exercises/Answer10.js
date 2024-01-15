9288423a55c5511938e0897725eb2b363cf53a06
Salih Yurek <salihyurek004@gmail.com>
2024-01-15 21:47:29 +0300

    01 day

--- 01_Day_Introduction/Exercises/Answer10.js
+++ 01_Day_Introduction/Exercises/Answer10.js
@@ -3,8 +3,14 @@
 // 10- Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
 // 10- myAge ve yourAge adında iki değişken tanımlayıp bunlara başlangıç değerleri atayın ve tarayıcı konsoluna giriş yapın.
 
+// I am 24 years old.
+// You are 30 years old.
+
 
 // Answer 
 
 let myAge = 24
 let yourAge = 32
+
+console.log(`I am ${myAge} years old.`)
+console.log(`You are  ${yourAge} years old.`)
