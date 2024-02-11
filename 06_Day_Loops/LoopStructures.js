98c0e928119c9d45a7f8e3257ed56abf8683b759
Salih Yurek <salihyurek@gmail.com>
2024-02-11 23:38:06 +0300

    06 day

---
+++ 06_Day_Loops/LoopStructures.js
@@ -0,0 +1,22 @@
+// For Loop Sructure
+
+/*
+for (initialization; condition; final-expression) {
+    code goes here
+} 
+*/
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i) // 0 1 2 3 4 5
+}
+
+for (let i = 5; i >= 0; i--) {
+    console.log(`${i} * ${i} = ${i * i}`) 
+}
+// 5 * 5 = 25
+// 4 * 4 = 16
+// 3 * 3 = 9
+// 2 * 2 = 4
+// 1 * 1 = 1
+// 0 * 0 = 0
+
